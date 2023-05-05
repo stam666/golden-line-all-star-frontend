8bba11b217d5f5be99b25483ef4c9816f89e85df
stam <stamp.chayakorn@gmail.com>
2023-05-05 20:53:22 +0700

    bot completed

--- src/pages/BotPage.jsx
+++ src/pages/BotPage.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 const BotPage = () => {
   const [text, setText] = useState('');
   const [result, setResult] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (event) => {
     setText(event.target.value);
@@ -12,16 +13,18 @@ const BotPage = () => {
 
   const sendText = async () => {
     try {
+      setIsLoading(true);
       const res = await axios.post(
-        `${Config.BACKEND_URL}/restaurants/menu`,
+        `${Config.BACKEND_URL}/api/v1/restaurants/menu`,
         { ingredient: text },
         {
           withCredentials: true,
         }
       );
 
-    //   console.log(res.data);
-      setResult(res.data);
+      console.log(res.data.data);
+      setResult(res.data.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -31,8 +34,14 @@ const BotPage = () => {
     <div className="botpage-container">
       <h3>What ingredient do you want to eat?</h3>
       <input value={text} onChange={handleChange} />
-      <button onClick={sendText}>Send</button>
-      {result !== '' && <h3 className="result">{result}</h3>}
+      <button disabled={isLoading} onClick={sendText}>Send</button>
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : result !== '' ? (
+        <h3 className="result">{result}</h3>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
